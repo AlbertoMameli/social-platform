5e4a19d4d2269400a9735ece7703552cee3ab293
AlbertoMameli <mamelialberto2@gmail.com>
2025-04-14 11:11:45 +0200

    feat: allineamento_testo

--- user.sql
+++ user.sql
@@ -10,13 +10,13 @@ SELECT `posts`.`id`,`posts`.`title`
 FROM `posts`
 LEFT JOIN `likes`
 ON `posts`.`id` = `likes`.`post_id`
-WHERE `likes`.`post_id` IS NULL
+WHERE `likes`.`post_id` IS NULL;
 --Conta il numero di like per ogni post (165 nel caso di query con SELECT + JOIN oppure 175 nel caso di query con sola SELECT)
 SELECT `posts`.`id`, `posts`.`title`, COUNT(`likes`.`post_id`) AS `total_likes`
 FROM `likes`
 RIGHT JOIN `posts`
 ON `likes`.`post_id` = `posts`.`id`
-GROUP BY `posts`.`id`
+GROUP BY `posts`.`id`;
 --Ordina gli utenti per il numero di media caricati (25) 
 SELECT `users`.`id`,`users`.`username`, COUNT(`medias`.`id`) AS `total_medias`
 FROM `users`
@@ -27,11 +27,11 @@ ON `posts`.`id` = `media_post`.`post_id`
 INNER JOIN `medias`
 ON `media_post`.`media_id` = `medias`.`id`
 GROUP BY `users`.`id`
-ORDER BY `total_medias` DESC
+ORDER BY `total_medias` DESC;
 --Ordina gli utenti per totale di likes ricevuti nei loro posts (25)
- SELECT `users`.`id`, `users`.`username`, COUNT(`likes`.`post_id`) AS `total_like_received`
+SELECT `users`.`id`, `users`.`username`, COUNT(`likes`.`post_id`) AS `total_like_received`
 FROM `users`
 INNER JOIN `likes`
 ON `users`.`id` = `likes`.`user_id`
 GROUP BY `users`.`id`
-ORDER BY  `total_like_received` DESC
+ORDER BY  `total_like_received` DESC;
